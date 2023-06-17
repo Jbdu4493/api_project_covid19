907dc843cbcea436750d96a15513d623973f9c28
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2023-06-17 10:12:36 +0200

    .DS_Store banished!

---
+++ main.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI, UploadFile, File
+import tensorflow as tf
+from PIL import Image
+import io
+import numpy as np
+
+# Charger le modèle TensorFlow pré-entraîné
+model = tf.keras.models.load_model("./models/model_ubnet_MobileNetV3Large.h5")
+
+# Créer l'instance de l'application FastAPI
+app = FastAPI()
+
+# Indice des classes
+idx_class = ['Covid', 'Lung Opacity', 'Normal', 'Viral Pneumonia']
+
+# Définir le point de terminaison pour l'API
+@app.post('/predict')
+async def predict(file: UploadFile = File(...)):
+    # Lire l'image à partir du fichier envoyé
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    
+    # Prétraitement de l'image
+    # Assurez-vous que l'image est redimensionnée à la taille attendue par le modèle (256x256x3)
+    image = image.resize((256, 256))
+    image_array = tf.keras.preprocessing.image.img_to_array(image)
+    image_array = tf.expand_dims(image_array, 0)  # Ajouter une dimension de lot (batch dimension)
+    
+    # Faire une prédiction avec le modèle TensorFlow
+    prediction = model.predict(image_array)[0]
+
+    idx_max_pred = prediction.argmax()
+    print(prediction,idx_max_pred)
+    # Convertir la prédiction en chaîne de caractères (à adapter selon votre modèle)
+    return {'prediction': f'{idx_class[idx_max_pred]}',"proba": f"{prediction[idx_max_pred]}"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
